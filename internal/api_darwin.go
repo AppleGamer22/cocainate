7280bb8f05acf284f38c3c5be960da404651e513
Omri Bornstein <omribor@gmail.com>
2022-01-06 13:58:33 +1100

    mac implementation

--- internal/api_darwin.go
+++ internal/api_darwin.go
@@ -1,35 +1,49 @@
 package internal
 
 import (
-	// "fmt"
-	"context"
+	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
-	"time"
+	"syscall"
 )
 
 func (session *Session) Start() error {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-	defer func() {
-		signal.Stop(signals)
-		cancel()
-	}()
-	// t := ""
-	// if duration > 0 {
-	// 	t = fmt.Sprintf("-t %s", duration)
-	// }
-	// w := ""
-	// if pid > 0 {
-	// 	w = fmt.Sprintf("-w %d", pid)
-	// }
-	cmd := exec.Command("caffeinate")
-	err := cmd.Run()
-	if err.Error() == "signal: interrupt" {
-		return nil
+	var args []string
+
+	if session.Duration > 0 {
+		args = append(args, "-t")
+		seconds := fmt.Sprintf("%d", int(session.Duration.Seconds()))
+		args = append(args, seconds)
 	}
+
+	if session.PID != 0 && session.PID != os.Getpid() {
+		args = append(args, "-w")
+		pid := fmt.Sprintf("%d", session.PID)
+		args = append(args, pid)
+	}
+
+	caffeinate := exec.Command("caffeinate", args...)
+	err := caffeinate.Start()
+	if err != nil {
+		return err
+	}
+
+	exits := make(chan error, 1)
+	if session.Duration > 0 {
+		go func() {
+			err := caffeinate.Wait()
+			exits <- err
+		}()
+	}
+
+	signals := make(chan os.Signal, 1)
+	go func() {
+		signal.Notify(signals, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+		<-signals
+		err := caffeinate.Process.Kill()
+		exits <- err
+	}()
+	err = <-exits
 	return err
 }
