a8b57f7e1adaddf12ba58f2b8386b8dbb5c1cfae
Omri Bornstein <omribor@gmail.com>
2022-02-17 13:56:17 +1100

    filters system processes on linux

--- cmd/ps.go
+++ cmd/ps.go
@@ -12,6 +12,34 @@ import (
 
 var all bool
 
+var blacklistedProcesses = []string{
+	"systemd",
+	"sddm-helper",
+	"pulseaudio",
+	"xdg-document-portal",
+	"gdm-session-worker",
+	"gdm-x-session",
+	"gnome-session-binary",
+	"dbus-daemon",
+	"at-spi-bus-launcher",
+	"ibus-daemon",
+	"gnome-shell",
+	"ibus-memconf",
+	"ibus-daemon",
+	"ibus-extension-gtk3",
+	"ibus-daemon",
+	"gvfsd-trash",
+	"gvfsd",
+	"evolution-alarm-notify",
+	"gnome-session-binary",
+	"ibus-engine-simple",
+	"ibus-daemon",
+	"gsd-disk-utility-notify",
+	"gnome-session-binary",
+	"plasma-browser-integration-host",
+	"nacl_helper",
+}
+
 var psCommand = &cobra.Command{
 	Use:   "ps",
 	Short: "list important processes",
@@ -27,7 +55,7 @@ var psCommand = &cobra.Command{
 			return err
 		}
 
-		writer := tabwriter.NewWriter(os.Stdout, 8, 8, 0, ' ', 0)
+		writer := tabwriter.NewWriter(os.Stdout, 8, 8, 1, ' ', 0)
 		defer writer.Flush()
 
 		n, err := fmt.Fprintf(writer, "%s\t%s\t%s\n", "PID", "PPID", "CMD")
@@ -37,8 +65,18 @@ var psCommand = &cobra.Command{
 
 		processNames := make(map[string]bool)
 		for _, p := range processes {
-			processUsername, err := p.Username()
-			if err != nil || processUsername != u.Username {
+			pid := p.Pid
+			curentPID := os.Getpid()
+			if pid == int32(curentPID) {
+				continue
+			}
+
+			parent, err := p.Parent()
+			if err != nil {
+				continue
+			}
+			parentName, err := parent.Name()
+			if err != nil {
 				continue
 			}
 
@@ -47,12 +85,27 @@ var psCommand = &cobra.Command{
 				continue
 			}
 
+			var blacklisted bool
+			for _, blacklistedProcess := range blacklistedProcesses {
+				if processName == blacklistedProcess || parentName == blacklistedProcess {
+					blacklisted = true
+					break
+				}
+			}
+			if blacklisted {
+				continue
+			}
+
+			processUsername, err := p.Username()
+			if err != nil || processUsername != u.Username {
+				continue
+			}
+
 			if _, ok := processNames[processName]; all || !ok {
 				processNames[processName] = true
 
-				pid := p.Pid
 				ppid, err := p.Ppid()
-				if err != nil {
+				if err != nil || ppid == 1 {
 					continue
 				}
 
