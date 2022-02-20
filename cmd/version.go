cd0007fb163d3980a51d7d4e0cea24fa8d0e381c
Omri Bornstein <omribor@gmail.com>
2022-02-20 11:25:00 +1100

    add github url to verbose version info

--- cmd/version.go
+++ cmd/version.go
@@ -19,7 +19,7 @@ var versionCommand = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if verbose {
 			fmt.Printf("version: \t%s\n", Version)
-			fmt.Printf("commit: \t%s\n", Hash)
+			fmt.Printf("commit: \thttps://github.com/AppleGamer22/cocainate/commit/%s\n", Hash)
 			fmt.Printf("compiler: \t%s (%s)\n", runtime.Version(), runtime.Compiler)
 			fmt.Printf("platform: \t%s/%s\n", runtime.GOARCH, runtime.GOOS)
 		} else {
