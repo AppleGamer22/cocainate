2070272efa3fca3abd6f5267d52964d7aa2a4152
Omri Bornstein <omribor@gmail.com>
2023-03-08 16:55:00 +1100

    progress % changes with second

--- progress_bar/model.go
+++ progress_bar/model.go
@@ -2,7 +2,6 @@ package progressbar_test
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
@@ -18,18 +17,20 @@ const (
 var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
 
 type model struct {
-	duration time.Duration
-	percent  float64
-	p        progress.Model
-	channel  chan bool
+	duration   time.Duration
+	amount     float64
+	percentage float64
+	p          progress.Model
+	channel    chan bool
 }
 
 func New(duration time.Duration) *model {
 	return &model{
-		duration: duration,
-		percent:  0.0,
-		p:        progress.New(progress.WithScaledGradient("#FF7CCB", "#FDFF8C")),
-		channel:  make(chan bool, 1),
+		duration:   duration,
+		amount:     float64(duration/time.Second) / float64(duration),
+		percentage: 0.0,
+		p:          progress.New(progress.WithScaledGradient("#FF7CCB", "#FDFF8C")),
+		channel:    make(chan bool, 1),
 	}
 }
 
@@ -42,19 +43,18 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		return m, tea.Quit
 	case tea.WindowSizeMsg:
-		m.p.Width = message.Width - padding*2 - 4
+		m.p.Width = message.Width
 		if m.p.Width > maxWidth {
 			m.p.Width = maxWidth
 		}
 		return m, nil
 	case time.Time:
-		m.percent += 0.25
-		if m.percent > 1.0 {
-			m.percent = 1.0
+		m.percentage += m.amount
+		if m.percentage > 1.0 {
+			m.percentage = 1.0
 			return m, tea.Quit
 		}
 		return m, tickCommand()
-
 	default:
 		return m, nil
 	}
@@ -62,8 +62,7 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	pad := strings.Repeat(" ", padding)
-	return fmt.Sprintf("\n%s%s\n\n%s%s", pad, m.p.ViewAs(m.percent), pad, helpStyle("Press any key to quit"))
+	return fmt.Sprintf("\n%s\n%s/%s\n%s", m.p.ViewAs(m.percentage), time.Duration(float64(m.duration)*m.percentage), m.duration, helpStyle("Press any key to quit"))
 }
 
 func tickCommand() tea.Cmd {
